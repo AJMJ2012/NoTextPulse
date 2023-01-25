fed74fb631e27b1f8c544529f5efbb34b5ca7794
Ashley Miller-Jelfs <ashley.miller.475@gmail.com>
2023-01-26 02:04:02 +1030

    Start

---
+++ NoTextPulse.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace NoTextPulse {
+	public class NoTextPulse : Mod {
+		public override void Load() {
+			if (Main.netMode == 2) { return; }
+			Main.mouseTextColor = 254;
+			On.Terraria.Main.DoUpdate_AnimateCursorColors += (DoUpdate_AnimateCursorColors) => {
+				Main.CursorColor();
+				Main.masterColor += (float)Main.masterColorDir * 0.05f;
+				if (Main.masterColor > 1f)
+				{
+					Main.masterColor = 1f;
+					Main.masterColorDir = -1;
+				}
+				if (Main.masterColor < 0f)
+				{
+					Main.masterColor = 0f;
+					Main.masterColorDir = 1;
+				}
+				return;
+			};
+			On.Terraria.Main.CursorColor += (CursorColor) => {
+				Main.cursorColor = Main.mouseColor;
+				return;
+			};
+		}
+	}
+}
